fceeece7793bd7c6890579916e8e546e25af8be8
Alex Blakes <alexander.blakes@manchester.ac.uk>
2024-08-02 10:40:32 +0100

    Update logging

--- src/boilerplate.py
+++ src/boilerplate.py
@@ -1,22 +1,19 @@
 """Boilerplate code for most modules."""
 
 import logging
-from pathlib import Path
 
 import pandas as pd
 
 import src
-
-_LOGFILE = f"data/logs/{Path(__file__).stem}.log"
 
 logger = logging.getLogger(__name__)
 
 
 def main():
     """Run as script."""
-    pass
+    return None
 
 
 if __name__ == "__main__":
-    logger = src.setup_logger(_LOGFILE)
+    logger = src.setup_logger(src.log_file(__file__))
     main()
